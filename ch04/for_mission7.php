ebf0cd4ffd7cd3eddfabf399aac16ba65ccebbd5
olnnl510 <101864135+olnnl510@users.noreply.github.com>
2022-05-23 15:57:39 +0900

    Add files via upload

---
+++ ch04/for_mission7.php
@@ -0,0 +1,15 @@
+<?php
+    $star = rand(1, 10);
+    for ($i=0; $i <= $star; $i++)
+    {
+        print "*" ;
+    }
+?>
+
+<!--
+    $star = rand(1, 10);
+    for ($i=*; $i<***********; *++)
+    {
+        print $star = $i ;
+    }
+-->
